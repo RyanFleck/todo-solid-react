3acee1f14ab9166145d34bd19afcc6070ee514e8
Ryan Fleck <ryan.fleck@protonmail.com>
2020-05-29 16:10:33 -0400

    Improve data model.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,56 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const fakeData = [
-  "plant thyme",
-  "lookup watering instructions for lavender",
-  "cut and replant mint",
+  {
+    text: "plant thyme",
+    done: false,
+  },
+  {
+    text: "lookup watering instructions for lavender",
+    done: false,
+  },
+  {
+    text: "cut and replant mint",
+    done: false,
+  },
+  {
+    text: "buy thyme",
+    done: true,
+  },
 ];
-
-const fakeDataDone = [];
 
 function App() {
   return (
     <div className="App">
+      {/* TODO section of the app. */}
       <h1>ToDo</h1>
-      {fakeData.map((x, key) => (
-        <div key={key} className={"todo-item"}>
-          <label className="todo-item-text">
-            <label className={"todo-item-label"}>
-              <input type={"checkbox"}></input>
-              <span className="pseudo-checkbox"></span>
-            </label>
-            {x}
-          </label>
-        </div>
-      ))}
+      {fakeData.map((elem, key) =>
+        !elem.done ? (
+          <CheckListItem key={key} text={elem.text} done={false} />
+        ) : null
+      )}
+
+      {/* DONE section of the app. */}
+      <h1>Done</h1>
+      {fakeData.map((elem, key) =>
+        elem.done ? (
+          <CheckListItem key={key} text={elem.text} done={true} />
+        ) : null
+      )}
+    </div>
+  );
+}
+
+function CheckListItem(props) {
+  return (
+    <div className={"todo-item"}>
+      <label className="todo-item-text">
+        <label className={"todo-item-label"}>
+          <input type={"checkbox"} defaultChecked={props.done}></input>
+          <span className="pseudo-checkbox"></span>
+        </label>
+        {props.text}
+      </label>
     </div>
   );
 }
