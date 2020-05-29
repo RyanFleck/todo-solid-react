a6c9f027a5b67648542f16bf14d86cc4426aae12
Ryan Fleck <ryan.fleck@protonmail.com>
2020-05-29 15:35:42 -0400

    Basic html/css for checkboxes.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const fakeData = [
+  "plant thyme",
+  "lookup watering instructions for lavender",
+  "cut and replant mint",
+];
+
+const fakeDataDone = [];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ToDo</h1>
+      {fakeData.map((x, key) => (
+        <div key={key} className={"todo-item"}>
+          <label className="todo-item-text">
+            <label className={"todo-item-label"}>
+              <input type={"checkbox"}></input>
+              <span className="pseudo-checkbox"></span>
+            </label>
+            {x}
+          </label>
+        </div>
+      ))}
     </div>
   );
 }
